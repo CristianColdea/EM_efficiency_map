c83a9967b5d8a4d0a6be5dc5396ead8c681755fc
CristianColdea <rdt333@gmail.com>
2022-09-18 20:38:19 +0300

    first script commit

---
+++ eem.py
@@ -0,0 +1,15 @@
+"""
+Script for Electric Motor Efficiency computation.
+"""
+
+def eem(t_max, omg_max, t, omg, type='IPM', t_const=False, P_const=False):
+    """
+    Function to compute efficiency based on regression.
+    Takes as inputs maximum torque, maximum speed/rpm, instantaneous torque and speed,
+    EM type, wether the motor works in constant torque or power regimes.
+    Outputs the EM efficiency on the given conditions.
+    """
+    if(t_const or P_const):  # if constant torque or power regimes
+        if(type == 'IPM'):
+            efficiency = 1
+        else: print("Two equation fitting is computed for IPM motor type only.")
